21fc08aabc8bfe96d5acb5e0b4209f6d1270c834
Ryan Lacon <ryan.lacon@gmail.com>
2019-08-06 16:23:16 -0400

    First commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', 0);
